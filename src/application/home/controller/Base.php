ef85a36a49c3c812bcb0447a16b48451beed2652
lakeui <zhangheng@weicantimes.com>
2019-01-03 17:23:50 +0800

    新增模板文件

---
+++ src/application/home/controller/Base.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\home\controller;
+use \think\Controller;
+/**
+ * Description of Base
+ *
+ * @author zhangheng
+ */
+class Base extends Controller {
+    //put your code here
+}
